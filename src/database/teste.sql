e9ba7c826402b407532e0f8d5b3fa07824508280
brzenit <bryan.lima@sptech.school>
2024-05-19 03:12:49 -0300

    Conectando com Web-data-viz

---
+++ src/database/teste.sql
@@ -0,0 +1,14 @@
+Create database TEste1;
+
+use Teste1;
+
+create table tacerto(
+banana int primary key,
+nome varchar(20)
+);
+
+insert into tacerto Values
+(1 , 'Nome'),
+(2 , 'Nome2');
+
+select * from tacerto;
